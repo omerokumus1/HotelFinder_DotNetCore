eb4d52e785b80acf71e0bb936cdc530dfc4cb670
Ömer Okumuş <omerokumus3@gmail.com>
2023-12-11 15:43:11 +0300

    Hotel entity props

--- HotelFinder.Entities/Hotel.cs
+++ HotelFinder.Entities/Hotel.cs
@@ -3,9 +3,16 @@ namespace HotelFinder.Entities
 {
 	public class Hotel
 	{
-		public Hotel()
-		{
-		}
-	}
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string City { get; set; }
+
+        public Hotel(int id, string name, string city)
+        {
+            Id = id;
+            Name = name;
+            City = city;
+        }
+    }
 }
 
