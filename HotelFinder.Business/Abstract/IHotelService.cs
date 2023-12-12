b1798798acdb31848292aec92b1429fb567cbaf2
Ömer Okumuş <omerokumus3@gmail.com>
2023-12-12 15:11:01 +0300

    Business layer

---
+++ HotelFinder.Business/Abstract/IHotelService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HotelFinder.Entities;
+namespace HotelFinder.Business.Abstract
+{
+	public interface IHotelService
+	{
+		List<Hotel> GetAllHotels();
+
+		Hotel GetHotelById(int id);
+
+		Hotel AddHotel(Hotel hotel);
+
+		Hotel UpdateHotel(Hotel hotel);
+
+		void DeleteHotel(int id);
+
+	}
+}
+
---
+++ HotelFinder.Business/Concrete/HotelManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using HotelFinder.Business.Abstract;
+using HotelFinder.Entities;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Concrete;
+
+namespace HotelFinder.Business.Concrete
+{
+	public class HotelManager: IHotelService
+	{
+        private IHotelRepository _hotelRepository;
+
+		public HotelManager(IHotelRepository hotelRepository)
+		{
+            _hotelRepository = hotelRepository;
+		}
+
+        public Hotel AddHotel(Hotel hotel)
+        {
+            return _hotelRepository.AddHotel(hotel);
+        }
+
+        public void DeleteHotel(int id)
+        {
+            _hotelRepository.DeleteHotel(id);
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return _hotelRepository.GetAllHotels();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            return _hotelRepository.GetHotelById(id);
+                
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            return _hotelRepository.UpdateHotel(hotel);
+        }
+    }
+}
+
